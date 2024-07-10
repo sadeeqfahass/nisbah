3327ad265e84bf775a580468fee80e32e65a5aab
sadeeq fahass <sadeeqfhassan@gmail.com>
2024-07-10 13:43:42 +0100

    Initial Commit

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Cards from './Cards'
+import Map from './Map'
+
+function Main() {
+  return (
+    <div className='main'>
+        <Cards />
+        <Map />
+    </div>
+  )
+}
+
+export default Main
---
+++ src/components/Map.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Map() {
+  return (
+    <div className='map-con'>
+      <div className='map' sx={{width: "100%"}}><iframe width="100%" height="570" frameborder="0"  src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Millenium%20City%20kaduna+(Nisbah)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">gps vehicle tracker</a></iframe></div>
+    </div>
+  )
+}
+
+export default Map
